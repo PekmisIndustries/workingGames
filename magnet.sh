9b7877ed5ec6b1024b48b7f2bb29ecd7e7be1228
Frostingold <36257251+Frostingold@users.noreply.github.com>
2024-06-28 20:19:36 +0200

    Removing useless crap & trying to add linux support (NOT TOTTALY IMPLEMENTED)

--- magnet.sh
+++ magnet.sh
@@ -2,10 +2,10 @@
 
 WG_FOLDER=$(dirname "$0")/wg
 
-ARIA2C_EXE_PATH=$(cat "${WG_FOLDER}/ariaPath.txt" | sed 's/\\/\//g')
+ARIA2C_EXE_PATH=aria2c
 MAGNET_URL=$(cat "${WG_FOLDER}/magnetToDownload.txt")
 
-DOWNLOAD_FOLDER=$(cygpath -u "${WG_FOLDER}/download")
+DOWNLOAD_FOLDER=("${WG_FOLDER}/download")
 
 "${ARIA2C_EXE_PATH}" --seed-time -d "${MAGNET_URL}" --dir "${DOWNLOAD_FOLDER}" 
 
--- main.py
+++ main.py
@@ -4,6 +4,12 @@ import time
 import shutil
 import subprocess
 
+def open_file(filename):
+    if sys.platform == "win32":
+        os.startfile("magnet.bat")
+    else:
+        os.system("exo-open --launch TerminalEmulator ./magnet.sh")
+
 
 def reset():
     if os.path.isfile("wg"):
@@ -11,10 +17,10 @@ def reset():
     print("reset done")
 
 def create_directories():
-    os.makedirs("wg\\game", exist_ok=True)
-    os.makedirs("wg\\path", exist_ok=True)
-    os.makedirs("wg\\download", exist_ok=True)
-    os.makedirs("wg\\unzipped", exist_ok=True)
+    os.makedirs("wg/game", exist_ok=True)
+    os.makedirs("wg/path", exist_ok=True)
+    os.makedirs("wg/download", exist_ok=True)
+    os.makedirs("wg/unzipped", exist_ok=True)
     return 0
 
 def get_skrakmi_file():
@@ -38,37 +44,37 @@ def create_game_folder(game_name):
     return 0
 
 def download_game_torrent(magnet_link):
-    aria_path = os.path.abspath("aria\\aria2c.exe")
-    save_path = os.path.abspath("wg\\download")
-    with open("wg\\ariaPath.txt", "w") as f:
+    aria_path = "dependancies/aria2c.exe"
+    save_path = os.path.abspath("wg/download")
+    with open("wg/ariaPath.txt", "w") as f:
         f.write(aria_path) 
-    with open("wg\\magnetToDownload.txt", "w") as m:
+    with open("wg/magnetToDownload.txt", "w") as m:
         m.write(magnet_link)
-    with open("wg\\downloadFolderPath.txt", "w") as n:
+    with open("wg/downloadFolderPath.txt", "w") as n:
         n.write(save_path)
     print("Created paths text files")
     print("Starting download process")
     #os.startfile("magnet.sh")
     #os.startfile("magnet.py")
-    os.startfile("magnet.bat")
+    open_file
 
 def move_downloaded_game():
-    for filename in os.listdir("wg\\download"):
-        file_path = os.path.join("wg\\download", filename)
-        shutil.move(file_path, "wg\\unzipped")
+    for filename in os.listdir("wg/download"):
+        file_path = os.path.join("wg/download", filename)
+        shutil.move(file_path, "wg/unzipped")
 
 def check_download_state():
     while True:
         time.sleep(2)
-        if(os.path.exists("wg\\download_complete.txt")):
-            os.remove("wg\\download_complete.txt")
+        if(os.path.exists("wg/download_complete.txt")):
+            os.remove("wg/download_complete.txt")
             return 0
         
 def check_unzipping_state():
     while True:
         time.sleep(2)
-        if(os.path.exists("wg\\unzipped.txt")):
-            os.remove("wg\\unzipped.txt")
+        if(os.path.exists("wg/unzipped.txt")):
+            os.remove("wg/unzipped.txt")
             return 0
 
 def apply_crack(crack_path, crack_destination):
@@ -81,8 +87,8 @@ def apply_crack(crack_path, crack_destination):
     print("crack applied")
 
 def move_game_to_personal_folder():
-    origin = os.path.abspath("wg\\unzipped")
-    destination = os.path.join("wg\\", sf[0])
+    origin = os.path.abspath("wg/unzipped")
+    destination = os.path.join("wg/", sf[0])
     for filename in os.listdir(origin):
         file_path = os.path.join(origin, filename)
         shutil.move(file_path, destination)
@@ -126,4 +132,4 @@ print("moving game")
 move_game_to_personal_folder()
 
 input("Game Ready!\nType something to launch the game!")
-os.startfile(os.path.join("wg\\", sf[0], sf[8]))
+os.startfile(os.path.join("wg/", sf[0], sf[8]))
